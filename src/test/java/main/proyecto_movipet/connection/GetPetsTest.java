fb7eeac24110fb9f98d48946013ae490e2771a2f
martin medina <medinanmartin@javeriana.edu.co>
2022-11-29 10:07:21 -0500

    21avo commit for the proyect

--- src/test/java/main/proyecto_movipet/connection/GetPetsTest.java
+++ src/test/java/main/proyecto_movipet/connection/GetPetsTest.java
@@ -1,5 +1,6 @@
 package main.proyecto_movipet.connection;
 
+import main.proyecto_movipet.model.Entidades.Mascota;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,13 @@ class GetPetsTest {
 
     @Test
     void asignarMascotas() {
-
+        GetPets getPets = new GetPets();
+        Mascota mascota = new Mascota();
+        mascota.setNombre("enzo");
+        mascota.setTipo("gato");
+        mascota.setRaza("N/R");
+        mascota.setEdad(2);
+        mascota.setComentarios("N/R");
+        mascota.setIdCliente(1000183553);
     }
 }
